9f70049666ae854e5053d9d2d818c6295039b4f6
Sazol-Sarker <sazolsarker1@gmail.com>
2024-06-14 19:59:57 +0600

    Scraped'

---
+++ script.js
@@ -0,0 +1,7 @@
+const newTxt = document.getElementById("text");
+// newTxt.innerHTML = "Bla bla bla";
+
+
+const newQuote = document.getElementById("newQuote");
+// fetch api response
+// const response=await 
